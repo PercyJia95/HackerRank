b81cf53d02c0675867c55dff4ce4e098eba7218f
Percy Jia <percy6995@gmail.com>
2019-10-18 20:29:06 -0700

    first commit

---
+++ Java/looping_and_skipping.sh
@@ -0,0 +1,16 @@
+temp=0
+while [ $temp -lt 100 ]
+do
+    if [ $((temp%2)) -eq 1 ];
+    then 
+        echo "$temp"
+    fi
+    temp=$((temp+1))
+done
+
+# TO be "Pythonic"
+
+# for number in {1..99..2}
+# do
+#     echo $number
+# done
